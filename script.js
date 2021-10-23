4d621a84627b740721270d7aa12658e8d45d2e00
Franco <72584397+erekkles@users.noreply.github.com>
2021-10-23 01:41:23 -0300

    Refactor and minimize all logic.
    I refactored the whole logic to make use of a single global array rather than just global variables, which is supported by the use of global variables, but could we reworked to just use scoped variables.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let first_number = 0;
 let second_number = 0;
-let last_number = 0;
-let current_operation;
-let last_operation;
+let result = 0;
+let current_operator;
+let evaluation = [];
 const screen = document.querySelector(".screen");
 const keyboard = document.querySelector(".keyboard");
+
+/*
+    TODO: Render and clear function pending of refactor
+    TODO: Handle styling when there appear bigger numbers
+    TODO: Check if it's possible to delete global variables and use scoped variables instead. 
+*/
 
 keyboard.addEventListener('click', function(e) {
     e.stopImmediatePropagation()
@@ -23,7 +29,7 @@ function onButtonPress (e) {
             Clear();
             break;
         case "equals":
-            Operate()
+            Operate();
             break;
     }
 
@@ -31,80 +37,101 @@ function onButtonPress (e) {
 }
 
 function AssignNumber(e) {
-    if (current_operation && first_number == 0) current_operation = null;
     
-    if (!current_operation) {
+    console.log(evaluation.length)
+    if(evaluation.length <= 1) {
         first_number = first_number == 0 
             ? parseInt(e.target.getAttribute("data-value"))
             : parseInt(first_number + e.target.getAttribute("data-value"))
 
-        last_number = first_number;
+        if(evaluation.length == 1) evaluation.shift();
+        evaluation.push(first_number)
+        result = first_number;
         return;
     }
 
-    second_number = second_number == 0
-        ? parseInt(e.target.getAttribute("data-value"))
-        : parseInt(second_number + e.target.getAttribute("data-value"))
+    if (evaluation.length >= 2) {
+        second_number = second_number == 0
+            ? parseInt(e.target.getAttribute("data-value"))
+            : parseInt(second_number + e.target.getAttribute("data-value"));
 
-    last_number = second_number;
+        if(evaluation.length == 3) evaluation.pop();
+        evaluation.push(second_number);
+        result = second_number;
+    }
 }
 
 function AssignOperation(e) {
-    if(!current_operation) return current_operation = e.target.getAttribute('data-value');
+    if(evaluation.length >= 1) {
+        current_operator = e.target.getAttribute('data-value');
 
-    last_operation = current_operation;
-    current_operation = e.target.getAttribute('data-value');
+        if(current_operator == "%" || current_operator == "+/-") return Operate();
+
+        if(evaluation.length == 3) Operate();
+        if(evaluation.length == 2) evaluation.pop();
+        evaluation.push(current_operator);
+        
+    }
 }
 
 function Clear() {
     if(second_number != 0) {
         second_number = 0;
-        last_number = first_number;
+        result = first_number;
         return;
     }
 
     first_number = 0;
-    last_number = 0;
+    result = 0;
 }
 
-/*
-    BUG SPOTTED: The render function renders on screen the last_number variable,
-    which after passing through the Operate function it doesn't actually sets itself
-    to the value but to the second_number, since its original functionality was 
-    to perform as a memory holder rather than the result holder. 
-
-    POSSIBLE FIX: Introduce a result variable. 
-*/
 function Operate() { 
-    /* Operations that can be only performed with two numbers */
-    if(first_number != 0 && second_number != 0) {
-        switch(current_operation) {
-            case "+":
-                first_number = first_number + second_number;
-                last_number = second_number;
-                second_number = 0;
-                break;
-        }
+
+    if(current_operator == "%") {
+        result = evaluation[0] / 100;
+        first_number = result; 
+        second_number = 0;
+        evaluation = [first_number];
+        return;
     }
+
+    if(current_operator == "+/-") {
+        result = evaluation[0] * -1;
+        first_number = result;
+        second_number = 0;
+        evaluation = [first_number]
+        return;
+    }
+
+    result = eval(evaluation.join().replace(/,/g, ""))
+    first_number = result;
+    second_number = 0;
+    evaluation = [first_number]
 }
 
 function Render() {
-    console.log(first_number)
     const clear_button = document.querySelector('div[data-button-type="clear"]');
 
-    let new_operation_button = Array.from(document.querySelectorAll('div[data-button-type="operator"]'))
-        .filter(button => button.getAttribute("data-value") == current_operation);
+    let new_operator_button = Array.from(document.querySelectorAll('div[data-button-type="operator"]'))
+        .filter(button => button.getAttribute("data-value") == current_operator);
 
-    let last_operation_button =  document.querySelector('.selected_operation');
+    let last_operator_button =  document.querySelector('.selected_operation');
 
     // Render in screen the latest number updated.
-    screen.textContent = last_number;
+    screen.textContent = result;
 
     second_number != 0
         ? clear_button.textContent = 'C'
         : clear_button.textContent = 'AC'
     
     // Change styling of selected operators.
-    last_operation_button ? last_operation_button.classList.remove('selected_operation') : null;
-    new_operation_button[0] ? new_operation_button[0].classList.add('selected_operation') : null;
+    last_operator_button ? last_operator_button.classList.remove('selected_operation') : null;
+    new_operator_button[0] ? new_operator_button[0].classList.add('selected_operation') : null;
 }
+
+// function debug() {
+//     console.log("first_number: " + first_number);
+//     console.log("second_number: " + second_number);
+//     console.log("current_operator: " + current_operator);
+//     console.log("end of log")
+// }
