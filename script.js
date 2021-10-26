227583bb2a6d7fa02f027d59ffc380a342d65f11
Franco <72584397+erekkles@users.noreply.github.com>
2021-10-26 15:09:26 -0300

    Refactor operate function, Render in progress
    Operate now is the handler for the equals and clear logic. Render function refactor is in progress. Added styles that will trigger when pressing operators.

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const screen = document.querySelector(".screen");
 const keyboard = document.querySelector(".keyboard");
 
 /*
-    TODO: Render and clear function pending of refactor
     TODO: Handle styling when there appear bigger numbers
-    TODO: Check if it's possible to delete global variables and use scoped variables instead. 
+    TODO: Remove selected_operation class when another button is pressed
+    TODO: Handle a way to introduce decimal numbers
 */
 
 keyboard.addEventListener('click', function(e) {
@@ -24,12 +24,6 @@ function onButtonPress (e) {
             break;
         case "operator":
             AssignOperation(e)
-            break;
-        case "clear":
-            Clear();
-            break;
-        case "equals":
-            Operate();
             break;
     }
 
@@ -57,74 +51,84 @@ function AssignNumber(e) {
         if(evaluation.length == 3) evaluation.pop();
         evaluation.push(second_number);
         result = second_number;
+
     }
 }
 
 function AssignOperation(e) {
-    if(evaluation.length >= 1) {
-        current_operator = e.target.getAttribute('data-value');
+    current_operator = e.target.getAttribute('data-value');
 
-        if(current_operator == "%" || current_operator == "+/-") return Operate();
+    // Exclusive operations that can be performed with one number, in the case of clear it can be executed even when the evaluation array is empty
+    if(current_operator == "%" || current_operator == "+/-" || current_operator == "clear" || current_operator == "=") return Operate();
 
-        if(evaluation.length == 3) Operate();
-        if(evaluation.length == 2) evaluation.pop();
-        evaluation.push(current_operator);
-        
-    }
-}
-
-function Clear() {
-    if(second_number != 0) {
-        second_number = 0;
-        result = first_number;
-        return;
-    }
-
-    first_number = 0;
-    result = 0;
+    if(evaluation.length == 3) Operate();
+    if(evaluation.length == 2) evaluation.pop();
+    evaluation.splice(1, 1, current_operator);
 }
 
 function Operate() { 
-
-    if(current_operator == "%") {
-        result = evaluation[0] / 100;
-        first_number = result; 
-        second_number = 0;
-        evaluation = [first_number];
+    if(current_operator == "%" && evaluation.length) {
+        result = evaluation[evaluation.length - 1] / 100;
+        evaluation.splice(evaluation.length - 1, 1, result);
         return;
     }
 
-    if(current_operator == "+/-") {
-        result = evaluation[0] * -1;
+    if(current_operator == "+/-" && evaluation.length) {
+        result = evaluation[evaluation.length - 1] * -1;
+        evaluation.splice(evaluation.length - 1, 1, result);
+        return;
+    }
+
+    if(current_operator == "clear") {
+        
+        if(evaluation.length <= 2) {
+            first_number = 0;
+            evaluation = [];
+            result = 0;
+            return;
+        }
+    
+        if(evaluation.length == 3) {
+            second_number = 0;
+            evaluation = [first_number]
+            result = first_number;
+            return;
+        }
+
+    }
+
+    if(evaluation.length == 3) {
+        result = eval(evaluation.join().replace(/,/g, ""))
+        console.log(result)
         first_number = result;
         second_number = 0;
         evaluation = [first_number]
-        return;
     }
-
-    result = eval(evaluation.join().replace(/,/g, ""))
-    first_number = result;
-    second_number = 0;
-    evaluation = [first_number]
 }
 
 function Render() {
-    const clear_button = document.querySelector('div[data-button-type="clear"]');
+    // select clear button
+    const clear_button = document.querySelector('div[data-value="clear"]');
 
+    // select operators buttons
     let new_operator_button = Array.from(document.querySelectorAll('div[data-button-type="operator"]'))
-        .filter(button => button.getAttribute("data-value") == current_operator);
+        .filter(button => button.getAttribute("data-value") == current_operator)[0];
 
-    let last_operator_button =  document.querySelector('.selected_operation');
+    let last_operator_button = document.querySelector('.selected_operation');
 
+    // change style of operators
+    last_operator_button ? last_operator_button.classList.remove('selected_operation') : null;
+    new_operator_button ? new_operator_button.classList.add('selected_operation') : null;
+
+
+    // render last number in screen
     screen.textContent = result;
 
-    second_number != 0
-        ? clear_button.textContent = 'C'
-        : clear_button.textContent = 'AC'
-    
-    // Change styling of selected operators.
-    last_operator_button ? last_operator_button.classList.remove('selected_operation') : null;
-    new_operator_button[0] ? new_operator_button[0].classList.add('selected_operation') : null;
+    // change clear button content
+    evaluation.length == 0
+        ? clear_button.textContent = 'AC'
+        : clear_button.textContent = 'C'
+
 }
 
 function debug() {
